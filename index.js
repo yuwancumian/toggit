edaaad0c5d21f2a1d1c3fc9c9836d5c7da9f7fa0
yuwancumian <yuwancumian@hotmail.com>
2018-01-01 11:10:29 +0800

    change dirname to pkgDir

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const path = require("path");
 const fs = require("fs-extra");
-
+const pkgDir = require("pkg-dir");
 const cac = require("cac");
 const cli = cac();
 
@@ -12,9 +12,9 @@ const defaultCommand = cli.command(
     "toggle file",
     (input,flags) => {
         const file = input[0];
-        const sourceFile = path.resolve(__dirname, file) 
-        const toggleFile = path.resolve(__dirname, ".tmp/" + file)
-        const distBack = path.resolve(__dirname, ".tmp/" + file + ".bak")
+        const sourceFile = path.resolve(pkgDir.sync(), file) 
+        const toggleFile = path.resolve(pkgDir.sync(), ".tmp/" + file)
+        const distBack = path.resolve(pkgDir.sync(), ".tmp/" + file + ".bak")
         fs.copySync(sourceFile,distBack)
         fs.copySync(toggleFile,sourceFile)
         fs.copySync(distBack,toggleFile)
