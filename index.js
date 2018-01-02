d940a0bc5b401c36cfe3201c71dc6cf0685cc7e7
yuwancumian <yuwancumian@hotmail.com>
2018-01-02 15:56:43 +0800

    backup file to root of .tmp

--- index.js
+++ index.js
@@ -4,20 +4,19 @@ const fs = require("fs-extra");
 const pkgDir = require("pkg-dir");
 const cac = require("cac");
 const cli = cac();
-
 const chalk = require("chalk");
 
 const defaultCommand = cli.command(
     "*",
     "toggle file",
     (input,flags) => {
-        const file = input[0];
-        const sourceFile = path.resolve(pkgDir.sync(), file) 
-        const toggleFile = path.resolve(pkgDir.sync(), ".tmp/" + file)
-        const distBack = path.resolve(pkgDir.sync(), ".tmp/" + file + ".bak")
-        fs.copySync(sourceFile,distBack)
-        fs.copySync(toggleFile,sourceFile)
-        fs.copySync(distBack,toggleFile)
+        const file = input[0].split("/").pop();
+        const sourceFile = path.resolve(pkgDir.sync(), input[0]); 
+        const toggleFile = path.resolve(pkgDir.sync(), ".tmp/" + file);
+        const distBack = path.resolve(pkgDir.sync(), ".tmp/" + file + ".bak");
+        fs.copySync(sourceFile,distBack);
+        fs.copySync(toggleFile,sourceFile);
+        fs.copySync(distBack,toggleFile);
         console.log(chalk.green("Toggle success!"))
     }
 )
